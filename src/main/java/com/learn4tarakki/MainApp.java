795a4448ee876629be038243d4131733b64335e2
learn4tarakki <git.learn4tarakki@gmail.com>
2018-05-06 18:41:30 +0530

    initial commit

---
+++ src/main/java/com/learn4tarakki/MainApp.java
@@ -0,0 +1,13 @@
+package com.learn4tarakki;
+
+public class MainApp {
+	
+	static int sum(int a, int b){
+		return a + b;
+	}
+
+	public static void main(String[] args) {
+		 float res = sum(12, 13);
+	}
+
+}
